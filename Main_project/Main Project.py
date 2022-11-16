6cffdc2f6532ae3ef26a83e673c74219959892c0
Anshul Sarathe <99202913+anshulsarathe35@users.noreply.github.com>
2022-11-16 10:02:01 +0530

    Update Main Project.py

--- Main_project/Main Project.py
+++ Main_project/Main Project.py
@@ -10,6 +10,7 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[0].id)
 
+
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
